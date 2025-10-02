0c9d13e003b7377a1750f83c0ca2b03753fa5342
adityachooramani <adityaxchooramani@gmail.com>
2025-10-02 14:44:04 +0530

    init commit

---
+++ js/preferred-lang.js
@@ -0,0 +1,1 @@
+(()=>{var a={fallback:"http://adityachooramani.github.io/",homes:{en:"http://adityachooramani.github.io/"}};(()=>{let o=navigator.language||navigator.userLanguage;if(o in a.homes){window.location.href=a.homes[o];return}let n=o.split("-");for(let i in a.homes)if(i.indexOf(n[0])===0){window.location.href=a.homes[i];return}window.location.href=a.fallback})();})();
